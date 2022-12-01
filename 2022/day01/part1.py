f0b665db7bb2d62131acbaa9c3c51925b10ff4e2
Yorik Timo Hansen <stu227160@informatik.uni-kiel.de>
2022-12-01 08:17:51 +0100

    Complete day1 part1

---
+++ 2022/day01/part1.py
@@ -0,0 +1,11 @@
+with open('input.txt') as f:
+    cur_sum = 0
+    max_sum = 0
+
+    for line in f:
+        if line[:-1] == '':
+            max_sum = max(max_sum, cur_sum)
+            cur_sum = 0
+        else:
+            cur_sum += int(line[:-1])
+    print(max_sum)
