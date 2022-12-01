902d3bd203da1b1bb91d2a070cc53054335809a3
Yorik Timo Hansen <stu227160@informatik.uni-kiel.de>
2022-12-01 08:28:05 +0100

    Fix part1

--- 2022/day01/part1.py
+++ 2022/day01/part1.py
@@ -2,7 +2,7 @@ with open('input.txt') as f:
     cur_sum = 0
     max_sum = 0
 
-    for line in f:
+    for line in f.readlines() + ['\n']:
         if line[:-1] == '':
             max_sum = max(max_sum, cur_sum)
             cur_sum = 0
