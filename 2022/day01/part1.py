15084deee0e17c1f05ee66b74713f119a69b2f04
Yorik Timo Hansen <stu227160@informatik.uni-kiel.de>
2022-12-01 16:38:05 +0100

    Some minor changes

--- 2022/day01/part1.py
+++ 2022/day01/part1.py
@@ -3,9 +3,9 @@ with open('input.txt') as f:
     max_sum = 0
 
     for line in f.readlines() + ['\n']:
-        if line[:-1] == '':
+        if line.strip() == '':
             max_sum = max(max_sum, cur_sum)
             cur_sum = 0
         else:
-            cur_sum += int(line[:-1])
+            cur_sum += int(line.strip())
     print(max_sum)
--- 2022/day01/part2.py
+++ 2022/day01/part2.py
@@ -1,13 +1,12 @@
 with open('input.txt') as f:
     cur_sum = 0
-    top3 = [0, 0, 0]
+    top = [0] * 3
 
     for line in f.readlines() + ['\n']:
-        if line[:-1] == '':
-            top3 += [cur_sum]
-            top3.remove(min(top3))
+        if line.strip() == '':
+            top += [cur_sum]
+            top.remove(min(top))
             cur_sum = 0
         else:
-            cur_sum += int(line[:-1])
-    print(cur_sum, top3)
-    print(sum(top3))
+            cur_sum += int(line.strip())
+    print(sum(top))
