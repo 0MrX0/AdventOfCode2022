7fe52bec84fd4298d3f6982e3f1f616804c92c79
Yorik Timo Hansen <stu227160@informatik.uni-kiel.de>
2022-12-04 12:03:42 +0100

    Add part2

---
+++ 2022/day04/part2.py
@@ -0,0 +1,11 @@
+with open('input.txt') as f:
+    duplicates = 0
+    for line in f.readlines():
+        a_desc, b_desc = line.split(',')
+        a_min, a_max = map(int, a_desc.split('-'))
+        b_min, b_max = map(int, b_desc.split('-'))
+        a = set(range(a_min, a_max + 1))
+        b = set(range(b_min, b_max + 1))
+        if len(a) + len(b) > len(a | b):
+            duplicates += 1
+    print(duplicates)
