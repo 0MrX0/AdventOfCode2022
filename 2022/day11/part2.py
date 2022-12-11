b66456696f28208be50f9a04e4e5b8321c673362
Yorik Timo Hansen <stu227160@informatik.uni-kiel.de>
2022-12-11 09:31:43 +0100

    Add day 11

---
+++ 2022/day11/part2.py
@@ -0,0 +1,42 @@
+import math
+
+with open('input.txt') as f:
+    monkeys = []
+    monkey = None
+    for line in f.readlines() + ['']:
+        match line.strip().split(' '):
+            case ['Monkey', _]:
+                monkey = {'inspections': 0}
+            case ['Starting', 'items:', *items]:
+                monkey['items'] = [int(i.replace(',', '')) for i in items]
+            case ['Operation:', *ops]:
+                monkey['operation'] = ' '.join(ops).split(' = ')[1]
+            case ['Test:', 'divisible', 'by', n]:
+                monkey['test'] = {'divisible': int(n)}
+            case ['If', x, 'throw', 'to', 'monkey', n]:
+                monkey['test'][x[:-1]] = int(n)
+            case _:
+                # test = monkey['test']
+                # monkey['test'] = lambda item: \
+                #     test['true'] if (item % test['divisible'] == 0) else test['false']
+
+                monkeys.append(monkey)
+                monkey = None
+
+    lcm = math.lcm(*[m['test']['divisible'] for m in monkeys])
+
+    for r in range(10000):
+        for i in range(len(monkeys)):
+            while monkeys[i]['items']:
+                monkeys[i]['inspections'] += 1
+                old = monkeys[i]['items'].pop(0)
+                item = eval(monkeys[i]['operation'])
+                item %= lcm
+
+                if item % monkeys[i]['test']['divisible'] == 0:
+                    monkeys[monkeys[i]['test']['true']]['items'].append(item)
+                else:
+                    monkeys[monkeys[i]['test']['false']]['items'].append(item)
+                # monkeys[monkeys[i]['test'](item)]['items'].append(item)
+    m = sorted([m['inspections'] for m in monkeys])[-2:]
+    print(m[0] * m[1])
