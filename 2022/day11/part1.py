a9cf7ea9496fd7c9bc01daf7b6e71f8aca3d5207
Yorik Timo Hansen <stu227160@informatik.uni-kiel.de>
2022-12-11 13:56:38 +0100

    Day 11 use lambda functions

--- 2022/day11/part1.py
+++ 2022/day11/part1.py
@@ -8,15 +8,16 @@ with open('input.txt') as f:
             case ['Starting', 'items:', *items]:
                 monkey['items'] = [int(i.replace(',', '')) for i in items]
             case ['Operation:', *ops]:
-                monkey['operation'] = ' '.join(ops).split(' = ')[1]
+                operation = ' '.join(ops).split(' = ')[1]
+                monkey['operation'] = lambda old, operation=operation: eval(operation)
             case ['Test:', 'divisible', 'by', n]:
                 monkey['test'] = {'divisible': int(n)}
             case ['If', x, 'throw', 'to', 'monkey', n]:
                 monkey['test'][x[:-1]] = int(n)
             case _:
-                # test = monkey['test']
-                # monkey['test'] = lambda item: \
-                #     test['true'] if (item % test['divisible'] == 0) else test['false']
+                test = monkey['test']
+                monkey['test'] = lambda item, test=test: \
+                    test['true'] if (item % test['divisible'] == 0) else test['false']
 
                 monkeys.append(monkey)
                 monkey = None
@@ -25,14 +26,10 @@ with open('input.txt') as f:
         for i in range(len(monkeys)):
             while monkeys[i]['items']:
                 monkeys[i]['inspections'] += 1
-                old = monkeys[i]['items'].pop(0)
-                item = eval(monkeys[i]['operation'])
+                item = monkeys[i]['items'].pop(0)
+                item = monkeys[i]['operation'](item)
                 item //= 3
 
-                if item % monkeys[i]['test']['divisible'] == 0:
-                    monkeys[monkeys[i]['test']['true']]['items'].append(item)
-                else:
-                    monkeys[monkeys[i]['test']['false']]['items'].append(item)
-                # monkeys[monkeys[i]['test'](item)]['items'].append(item)
+                monkeys[monkeys[i]['test'](item)]['items'].append(item)
     m = sorted([m['inspections'] for m in monkeys])[-2:]
     print(m[0] * m[1])
