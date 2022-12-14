6685ecdf3e29d747e7c7d5b95e9f4d24c841ea74
Yorik Timo Hansen <stu227160@informatik.uni-kiel.de>
2022-12-14 09:16:27 +0100

    Add day 14

---
+++ 2022/day14/part1.py
@@ -0,0 +1,46 @@
+with open('input.txt') as f:
+    rock = [list(tuple(map(int, q.strip().split(','))) for q in p.split('->')) for p in f]
+    x_range = (min(min(x[0] for x in p) for p in rock), max(max(x[0] for x in p) for p in rock))
+    y_range = 0, max(max(q[1] for q in p) for p in rock)
+
+    occupied = set()
+    for p in rock:
+        for q in range(len(p) - 1):
+            xa, ya = p[q]
+            xb, yb = p[q + 1]
+            while xa != xb or ya != yb:
+                occupied.add((xa, ya))
+                if xa < xb:
+                    xa += 1
+                elif xa > xb:
+                    xa -= 1
+                if ya < yb:
+                    ya += 1
+                elif ya > yb:
+                    ya -= 1
+        occupied.add(p[-1])
+
+
+    generating = True
+    i = 0
+    while generating:
+        x, y = 500, 0
+        stop = False
+        while not stop:
+            if (x, y + 1) not in occupied:
+                y += 1
+            elif (x - 1, y + 1) not in occupied:
+                x -= 1
+                y += 1
+            elif (x + 1, y + 1) not in occupied:
+                x += 1
+                y += 1
+            else:
+                occupied.add((x, y))
+                stop = True
+                i += 1
+            if y > y_range[1]:
+                stop = True
+                generating = False
+
+    print(i)
